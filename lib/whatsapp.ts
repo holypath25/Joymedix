53eca66c5498cde19d53eebdd02f2d4a1b74f74a
holypath25 <holypath25@gmail.com>
2026-07-28 22:13:48 +0900

    Delete lib directory

--- lib/whatsapp.ts
+++
@@ -1,6 +0,0 @@
-import { WHATSAPP_NUMBER, WHATSAPP_DEFAULT_MESSAGE } from './constants'
-
-export function getWhatsAppUrl(message?: string): string {
-  const msg = encodeURIComponent(message ?? WHATSAPP_DEFAULT_MESSAGE)
-  return `https://wa.me/${WHATSAPP_NUMBER}?text=${msg}`
-}
